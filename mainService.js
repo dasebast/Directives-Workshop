f324473b2dd7e1ec268f5ac6675a581c6f39f525
Doug Sebastian <dasebast@gmail.com>
2015-01-28 22:56:16 -0700

    notifications not working fully

--- mainService.js
+++ mainService.js
@@ -4,7 +4,7 @@ app.service('mainService', function($http){
 	this.getData = function(artist) {
         return $http({'method': 'JSONP', 'url': 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK' })
             .then(function (data) {
-                return data;
+                return data.data.results;
             });
     };
 })
--- myDirectives.js
+++ myDirectives.js
@@ -1,15 +1,73 @@
 var app = angular.module('directiveWorkshop');
 
-app.directive('pending', function() {
+app.directive('pending', function($q) {
 
 	return {
 		restrict: 'A',
 		scope: {
 			request: '&'
 		},
+		// templateUrl: 'myDirectives.html',
 		link: function(scope, elem, attrs) {
-			
-		}
+			var spinnerIcon = angular.element('<img style="height: 25px" src="spinner.GIF" />')
+			spinnerIcon.hide();
+			elem.after(spinnerIcon);
+
+			var invokeRequest = function () {
+				var dfd = $q.defer();
+
+				dfd.resolve(scope.request());
+
+				return dfd.promise
+			}
+
+			elem.click(function(){
+				elem.hide();
+				spinnerIcon.show();
+				invokeRequest().then(function(){
+					setTimeout(function(){
+						elem.show();
+						spinnerIcon.hide();
+					}, 2000);
+				});
+				
+			})
+					
+		},
+		
 	}
+
+});
+
+app.directive('notify', function() {
+	return {
+		restrict: 'A',
+		scope: {
+			title: '=',
+			body: '=',
+			icon: '@'
+		},
+		link: function(scope, elem, attrs) {
+			console.log(scope);
+			var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+		    Notification.requestPermission(function (permission) { });
+
+			var show = function() {
+				var notification = new Notification(scope.title, { body: scope.body })
+			}
+
+			elem.click(function() {
+				debugger;
+				show();
+		                //console.log(permission);
+		       	
+		                
+		     }); //close elem.click
+
+			
+			
+
+		} //close link
+	} //close return
 
 });
